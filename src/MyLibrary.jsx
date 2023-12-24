a0f684cf1eb1e2b0f51761bd016f44d76374db9f
Alejandro Leonian <alejandromleonian@gmail.com>
2023-12-24 16:41:13 -0300

    added ./dist to .gitignore

--- src/MyLibrary.jsx
+++ src/MyLibrary.jsx
@@ -7,22 +7,22 @@ export const MyLibrary = () => {
 
 export const Ace = () => {
   return (
-    <div class="card card-a" style="color: #333;">
-      <div class="card-middle">
-        <div class="card-suit" style="left: 50%; top: 50%; transform: translate(-50%, -50%) rotate(0)">
+    <div className="card card-a" style="color: #333;">
+      <div className="card-middle">
+        <div className="card-suit" style="left: 50%; top: 50%; transform: translate(-50%, -50%) rotate(0)">
         <img src="./assets/fa/spade.svg" />
         </div>
       </div>
-      <div class="card-corner card-topleft">
-        <div class="card-rank">A</div>
-        <div class="card-suit">
+      <div className="card-corner card-topleft">
+        <div className="card-rank">A</div>
+        <div className="card-suit">
           <img src="./assets/fa/spade.svg" />
 
         </div>
       </div>
-      <div class="card-corner card-bottomright">
-        <div class="card-rank">A</div>
-        <div class="card-suit">
+      <div className="card-corner card-bottomright">
+        <div className="card-rank">A</div>
+        <div className="card-suit">
           <img src="./assets/fa/spade.svg" />
         </div>
       </div>
