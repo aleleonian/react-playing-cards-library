11921bf7ffe37913fc7cfc95316e2d1485d1a8e1
Alejandro Leonian <alejandromleonian@gmail.com>
2023-12-25 11:10:43 -0300

    working on it

--- src/MyLibrary.jsx
+++ src/MyLibrary.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import './assets/css/MyLibrary.css';
 
+//TODO: this fixed the fact that now the custom classes are being applied to the app
+// but something in the build process breaks the names of the classes by adding spaces
+// between characters
+import styles from './assets/css/MyLibrary.css';
 
 export const MyLibrary = () => {
   return (
     <div>
       This is my React library yea!!
-      {/* <img src="./assets/spade.svg" /> */}
     </div>
   )
 };
 
 export const Ace = () => {
   return (
-    <div className="card card-a">
-      <div className="card-middle">
-        <div className="card-suit">
+    <div className={`${styles.card}`}>
+
+      <div className={styles.card-middle}>
+        <div className={styles.suit}>
           <img src={require('./assets/fa/spade.svg').default} />
         </div>
       </div>
-      <div className="card-corner card-topleft">
-        <div className="card-rank">A</div>
+      <div className={`${styles.corner} ${styles.card-topleft}`}>
+        <div className={styles.card-rank}>A</div>
         <div className="card-suit">
           <img src={require('./assets/fa/spade.svg').default} />
         </div>
